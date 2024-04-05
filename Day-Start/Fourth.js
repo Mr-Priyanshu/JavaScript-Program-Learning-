5f229de87cc0a777d412519319997763880893c9
Mr Priyanshu Garg <88274098+Mr-Priyanshu@users.noreply.github.com>
2024-04-05 12:45:53 +0000

    operator

---
+++ Day-Start/Fourth.js
@@ -0,0 +1,42 @@
+// *************Operations******************
+let yog1 = 23;
+let yog2 = 87;
+
+let yog = yog1+yog2;
+console.log(yog); 
+// --------------------------
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2%2);
+// ---------------------------------
+console.log("23"+3*9+3);
+// This Console Operation Output is a 23273  only * opration run 
+
+// Total Operator in Java script 
+/*
+An assignment operator assigns a value to its left operand based on the value of its right operand. The simple assignment operator is equal (=),
+ which assigns the value of its right operand to its left operand. That is, x = f() is an assignment expression that assigns the value of f() to x.
+*/
+
+
+//      Name	                Shorthand operator	                 Meaning
+// ---------------------------------------------------------------------------------------
+// Assignment	                    x = f()	                        x = f()
+// Addition assignment	            x += f()	                    x = x + f()
+// Subtraction assignment	        x -= f()	                    x = x - f()
+// Multiplication assignment	    x *= f()	                    x = x * f()
+// Division assignment	            x /= f()	                    x = x / f()
+// Remainder assignment	            x %= f()	                    x = x % f()
+// Exponentiation assignment	    x **= f()	                    x = x ** f()
+// Left shift assignment	        x <<= f()	                    x = x << f()
+// Right shift assignment	        x >>= f()	                    x = x >> f()
+// Unsigned right shift assignment	x >>>= f()	                    x = x >>> f()
+// Bitwise AND assignment	        x &= f()	                    x = x & f()
+// Bitwise XOR assignment	        x ^= f()	                    x = x ^ f()
+// Bitwise OR assignment	        x |= f()	                    x = x | f()
+// Logical AND assignment	        x &&= f()	                    x && (x = f())
+// Logical OR assignment	        x ||= f()	                    x || (x = f())
+// Nullish coalescing assignment	x ??= f()	                    x ?? (x = f())
+
