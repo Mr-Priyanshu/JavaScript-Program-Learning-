90dcff08901343ab2ed339d76a5c12c216ed699d
Mr Priyanshu Garg <88274098+Mr-Priyanshu@users.noreply.github.com>
2024-04-05 10:27:55 +0000

    Second Day Know her Data Type

---
+++ Day-Start/Second.js
@@ -0,0 +1,24 @@
+// Second part Of java script 
+"use strict"  // This is use to updated js only use
+
+// Check Documentation and file erite some code 
+
+console.log(3+3);
+
+
+// Data Type in js file 
+
+// primitce data type
+
+//number => 2 
+//bigint
+//String => ""
+//boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol 
+
+//object (non primitive )
+
+console.log(typeof null);
+console.log(typeof undefined);
