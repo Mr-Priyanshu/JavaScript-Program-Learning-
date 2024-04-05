6ae50131a0430d775c08e723694c4bab12feb385
Mr Priyanshu Garg <88274098+Mr-Priyanshu@users.noreply.github.com>
2024-04-05 11:03:04 +0000

    Converson

---
+++ Day-Start/Third.js
@@ -0,0 +1,17 @@
+//Converson Of Data Type 
+
+let score = "23";  // This is a String Type Data 
+
+console.log(score);
+console.log(typeof (score))
+// Tuype of String 
+
+let valueInNumber = Number(score) // Convert string to Number nut some problem Somtime this function Show you NaN Not a Number 
+
+// "33" => 33
+//"33acd" => Nam {Not a Number}
+
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
